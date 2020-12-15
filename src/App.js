00c3fe9125c7b3f1f0013aba0ce6f02b80b78e1f
Chris Rumler <cgrumler@gmail.com>
2020-12-15 13:50:00 -0500

    initial v2 push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// Import of Components
+import MyNavbar from './components/navbar/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar />
+      My Portfolio Here!
+    </div>
+  );
+}
+
+export default App;
