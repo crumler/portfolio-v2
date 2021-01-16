845860d7e876eac9170640624a20414fbb07e1b3
Chris Rumler <cgrumler@gmail.com>
2021-01-16 10:41:32 -0500

    resume update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
                                         </a>
                                     </div>
                                     <div>
-                                        <a href="https://drive.google.com/file/d/1r3pLPGzmZNVx2O-a12YqrbfNphKrWJAr/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                                        <a href="https://drive.google.com/file/d/1MPbBXQVE6G8ik0UwBUwbVsMzDGStPXgr/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                                             <Button className="m-2" variant="outline-success">
                                                 My Resume
                                             </Button>
